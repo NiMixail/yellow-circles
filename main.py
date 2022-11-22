1e95b7988020c7d15e7b7e800091f76dd51ed3a1
nimixail <naribmax@yandex.ru>
2022-11-22 13:13:45 +0400

    Круги теперь случайных цветов. Интерфейс подключается классом.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import sys
 from random import randint
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 
+from ui import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -16,9 +17,9 @@ class MyWidget(QMainWindow):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
             for _ in range(5):
                 diam = randint(50, 300)
+                qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
                 qp.drawEllipse(randint(100, 500), randint(50, 300), diam, diam)
             qp.end()
 
